b64ce8a4cc7586ecc9815cd6d7e13a6243c7d1e7
Your Name <you@example.com>
2025-10-23 14:49:12 +0800

    删除

--- packages/amis-app/prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  tabWidth: 2,
-  bracketSpacing: true,
-  arrowParens: 'always'
-};
--- packages/amis-app/src/setupTests.ts
+++
@@ -1,2 +0,0 @@
-// Tests setup placeholder. Additional global mocks can be added here if needed.
-export {};
